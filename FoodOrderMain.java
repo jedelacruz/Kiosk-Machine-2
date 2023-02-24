cf123cc95547e23fa11d9c1195b2092763790bd8
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-02-24 09:10:02 +0800

    Create FoodOrderMain.java

---
+++ FoodOrderMain.java
@@ -0,0 +1,11 @@
+package restoFinalRework;
+
+public class FoodOrderMain {
+	public static void main(String[] args) {
+		
+		FoodOrderSub connect = new FoodOrderSub();
+		
+		connect.head();
+		
+	}
+}
